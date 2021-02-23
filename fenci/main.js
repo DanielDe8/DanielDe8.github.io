1a090b2a3b71e3db51553c30b9e4d8151cc62f35
DanikD <daniel.devecka@zaci.zsjilove.cz>
2021-02-23 10:24:53 +0100

    zacatek fullsizu

---
+++ fenci/main.js
@@ -0,0 +1,9 @@
+var isFullscreen = [false, false, false, false, false, false]
+
+function fullscreen(id) {
+    id.style.width = "100%"
+    //document.write('<img src="' + id.src + '" id="fullimg" width="72%"')
+
+    //document.getElementById("fullimg").style.width = "72%";
+    //document.getElementById("fullimg").style.textAlign = "center";
+}
